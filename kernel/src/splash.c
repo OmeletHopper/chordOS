99841dea0b8a8180bd7e1fda88cb3af3c30fcaae
Jonny Archer <me@jonnyarcher.com>
2018-09-30 09:57:27 -0500

    Add splash.

---
+++ kernel/src/splash.c
@@ -0,0 +1,9 @@
+const char *splash = "\n\
+xxx   XX   xxx XX   xxx   XXX   XX\n\
+xxx   XX   xxx XX   xxx   XXX   XX\n\
+xxx   XX   xxx XX   xxx   XXX   XX\n\
+xxx   XX   xxx XX   xxx   XXX   XX\n\
+xxxx XXXX xxxx XXXX xxxx XXXX XXXX\n\
+xxxx XXXX xxxx XXXX xxxx XXXX XXXX\n\
+xxxx XXXX xxxx XXXX xxxx XXXX XXXX\n\
+xxxx XXXX xxxx XXXX xxxx XXXX XXXX\n";

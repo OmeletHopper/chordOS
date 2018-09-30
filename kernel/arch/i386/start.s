babc04a44f1c61d74fbb2686836f8806dc3534fc
Jonny Archer <me@jonnyarcher.com>
2018-09-30 13:31:03 -0500

    Disable flashing colors.

--- kernel/arch/i386/start.s
+++ kernel/arch/i386/start.s
@@ -4,6 +4,20 @@ extern kernel_main
 
 global start
 start:
+          ; Clear Interrupts.
           cli
+          ; Disable flashing colors.
+          mov dx, 0x3DA
+          in al, dx
+          mov dx, 0x3C0
+          mov al, 0x30
+          out dx, al
+          inc dx
+          in al, dx
+          and al, 0xF7
+          dec dx
+          out dx, al
+          ; Call our main C code.
           call kernel_main
+          ; Infinite loop.
           jmp $
